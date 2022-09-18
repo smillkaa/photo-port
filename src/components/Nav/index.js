41357564af6d366dba05eb6bb0867770a07aad63
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-09-17 23:33:25 -0700

    navigation

---
+++ src/components/Nav/index.js
@@ -0,0 +1,43 @@
+import { capitalizeFirstLetter} from "../../utils/helpers"
+import React, { useEffect } from 'react'
+
+function Nav(props) {
+
+    const { categories = [], setCurrentCategory, currentCategory } = props
+    useEffect(() => {
+        document.title = capitalizeFirstLetter(currentCategory.name)
+    }, [currentCategory])
+
+    return (
+        <header className="felx-row px-1">
+            <h2>
+                <a data-testid="link" href="/">
+                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                <li className="mx-2">
+                    <a href="#about">
+                    About me
+                    </a>
+                </li>
+                <li>
+                    <span>Contact</span>
+                </li>
+                {categories.map((category) => (
+                    <li
+                    className={`mx-1 ${ currentCategory.name === category.name && 'navActive' }`}
+                    key={category.name}>
+                        <span onClick={() => {setCurrentCategory(category)}} >
+                            {capitalizeFirstLetter(category.name)}
+                        </span>
+                    </li>
+                ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav
